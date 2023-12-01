26f739c2ea55b04d6fc021a8dd1f1a4f1f3241e7
Mila Berni <mila.berni@agilelab.it>
2023-12-01 13:37:34 +0100

    refactor tests

--- day_1/main.py
+++ day_1/main.py
@@ -1,6 +1,7 @@
 
 from file_reader.file_reader import FileReader
 import logging, sys
+import re
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
@@ -10,21 +11,80 @@ logger.addHandler(stream_handler)
 class Day1Solver(FileReader):
   
   def solve_first_problem(self, file_name: str) -> int:
+    """
+    Given a file, it returns the sum of the first and
+    last digits found in each line.
+    """
     lines = self.get_lines(file_name)
     logger.debug(f'Read these lines: {lines}')
 
     numbers = []
     for line in lines:
-      only_digits = [char for char in line if char.isdigit()]
+      only_digits = self._get_all_digits(line)
       logger.debug(f'Found these digits: {only_digits}')
-      numbers.append(f'{only_digits[0]}{only_digits[-1]}')
-    logger.info(f'Got this numbers: {numbers}')
+      numbers.append(self._compose_number(only_digits))
+    logger.debug(f'Got this numbers: {numbers}')
     result = sum([int(number) for number in numbers])
     logger.info(f'The sum of the numbers is {result}')
 
     return result
+  
+
+  def _get_all_digits(self, line: str) -> list[int]:
+    return [int(char) for char in line if char.isdigit()]
+  
+  def _compose_number(self, number_sequence: list[int]) -> int:
+    return int(f'{number_sequence[0]}{number_sequence[-1]}')
+  
+  def solve_second_problem(self, file_name: str) -> int:
+    """
+    Given a file name, it returns the sum of the numbers as words
+    found in each line.
+    """
+
+    lines = self.get_lines(file_name)
+    logger.debug(f'Read these lines: {lines}')
+
+    numbers = []
+    for line in lines:
+      only_digits = self._get_all_digits(self._replace_numbers(line))
+      logger.debug(f'Found these digits: {only_digits}')
+      numbers.append(self._compose_number(only_digits))
+    logger.debug(f'Got this numbers: {numbers}')
+    result = sum([int(number) for number in numbers])
+    logger.info(f'The sum of the numbers is {result}')
+
+    return result
+  
+  
+  def _replace_numbers(self, line: str) -> str:
+    """
+    Given a line, it finds the first occurrence of a number as word
+    and returns the number found and the rest of the line.
+    """
+
+    NUMBERS = {
+      'one': 1,
+      'two': 2,
+      'three': 3,
+      'four': 4,
+      'five': 5, 
+      'six': 6,
+      'seven': 7,
+      'eight': 8,
+      'nine': 9,
+    }
+
+    logger.info(f'Replacing in line: {line}')
+    modified_line = line
+    for num in NUMBERS.keys():
+      modified_line = modified_line.replace(num, str(NUMBERS[num]))
+    logger.info(f'Line {line} became {modified_line}')
+
+    return modified_line
 
 
 
 if __name__ == '__main__':
-  Day1Solver().solve_first_problem("day_1/input.txt")
+  Day1Solver().solve_first_problem("day_1/input_1.txt")
+  Day1Solver().solve_second_problem("day_1/input_2.txt")
--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -6,12 +6,37 @@ from day_1.main import Day1Solver
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 logger.addHandler(stream_handler)
 
 
 class TestDay1Solver(unittest.TestCase):
 
-  def test_can_find_digits(self):
-    result = Day1Solver().solve_first_problem("tests/resources/test_day_1.txt")
+  def test_can_solve_first_problem(self):
+    result = Day1Solver().solve_first_problem("tests/resources/test_day_1_1.txt")
     self.assertEqual(result, 142)
+
+  def test_can_solve_second_problem(self):
+    result = Day1Solver().solve_second_problem("tests/resources/test_day_1_2.txt")
+    self.assertEqual(result, 281)
+
+  def test_can_return_only_digits(self):
+    line = 'pqr3stu8vwx'
+    digits = Day1Solver()._get_all_digits(line)
+    logger.debug(f'Digits found: {digits}')
+    self.assertEqual(digits, [3, 8])
+
+  def test_creates_correct_number(self):
+    digits_list = [4, 2, 7]
+    self.assertEqual(Day1Solver()._compose_number(digits_list), 47)
+
+  def test_replaces_numbers_with_correct_digits(self):
+    line = '4nineeightseven2'
+    modified_line = Day1Solver()._replace_numbers(line)
+    logger.debug(f'Line {line} became {modified_line}')
+    self.assertEqual(modified_line, '49872')
+
+    another_line = 'zoneight234'
+    another_modified_line = Day1Solver()._replace_numbers(another_line)
+    logger.debug(f'Line {another_line} became {another_modified_line}')
+    self.assertEqual(another_modified_line, 'z1ight234')
