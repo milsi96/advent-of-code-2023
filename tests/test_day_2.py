3a3b9a2d27265c25daa7439851b62da103c9d4cb
Mila Berni <mila.berni@agilelab.it>
2023-12-03 02:54:34 +0100

    add day 2 solution

---
+++ tests/test_day_2.py
@@ -0,0 +1,21 @@
+import unittest
+import logging
+import sys
+
+from day_2.main import Day2Solver
+
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.setLevel(logging.INFO)
+logger.addHandler(stream_handler)
+
+
+class TestDay2Solver(unittest.TestCase):
+
+  def test_can_solve_first_problem(self):
+    result = Day2Solver().solve_first_problem('tests/resources/test_day_2_1.txt')
+    self.assertEqual(result, 8)
+
+  def test_can_solve_second_problem(self):
+    result = Day2Solver().solve_second_problem('tests/resources/test_day_2_1.txt')
+    self.assertEqual(result, 2286)
