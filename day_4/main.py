cd2346004d52d0cb164a849c2893571eb2f4bd5d
Mila Berni <mila.berni@agilelab.it>
2023-12-05 09:45:54 +0100

    add second problem solution

--- day_4/main.py
+++ day_4/main.py
@@ -36,6 +36,52 @@ class Card:
     def __str__(self):
         return f'Card {self.id}: {self.winning_numbers} | {self.numbers};'
 
+    @property
+    def wins(cls) -> int:
+        matches = 0
+        for num in cls.numbers:
+            if num in cls.winning_numbers:
+                matches = matches + 1
+        return matches
+
+
+class CardPile:
+    cards: dict[int, Card] = {}
+
+    def __init__(self, cards: list[Card]):
+        for card in cards:
+            logger.info(f'Inserting card {card}')
+            self.cards[card.id] = card
+
+    @property
+    def total_scratchpads(cls) -> int:
+        total = 0
+        for index in cls.cards.keys():
+            total = total + cls.get_card_wins(index)
+        return total + len(cls.cards.keys())
+
+    def get_card_wins(self, index: int, statistics: dict[int, int] = {}) -> int:
+        if self.cards.get(index).wins == 0:
+            statistics[index] = 0
+            return 0
+
+        if index in statistics.keys():
+            return statistics.get(index)
+
+        LOWER_LIMIT = index + 1
+        UPPER_LIMIT = index + self.cards.get(index).wins + 1
+
+        current_total = UPPER_LIMIT - LOWER_LIMIT
+        logger.debug(f'Won {current_total} scratchpads for id {index} for now')
+        for target in range(LOWER_LIMIT, UPPER_LIMIT):
+            current_total = current_total + self.get_card_wins(target, statistics)
+
+        statistics[index] = current_total
+
+        logger.debug(statistics)
+
+        return statistics[index]
+
 
 class Day4Solver(FileReader):
     def solve_first_problem(self, file_name: str) -> int:
@@ -44,7 +90,6 @@ class Day4Solver(FileReader):
         for line in lines:
             logger.info(f'Reading line: {line}')
             cards.append(self.parse_card(line))
-            logger.debug(str(cards[-1]))
 
         final_value = sum([card.card_value for card in cards])
         logger.info(f'Cards are worth {final_value}')
@@ -72,9 +117,13 @@ class Day4Solver(FileReader):
         return Card(card_id, numbers, winning_numbers)
 
     def solve_second_problem(self, file_name: str) -> int:
-        self.get_lines(file_name)
+        lines = FileReader().get_lines('day_4/input_1.txt')
+        cards = [self.parse_card(line) for line in lines]
+        pile = CardPile(cards)
+
+        logger.info(f'Total scratchcards: {pile.total_scratchpads}')
 
 
 if __name__ == '__main__':
     Day4Solver().solve_first_problem("day_4/input_1.txt")
-    # Day4Solver().solve_second_problem("day_4/input_2.txt")
+    Day4Solver().solve_second_problem("day_4/input_2.txt")
--- tests/test_day_4.py
+++ tests/test_day_4.py
@@ -2,7 +2,8 @@ import unittest
 import logging
 import sys
 
-from day_4.main import Card, Day4Solver
+from day_4.main import Card, CardPile, Day4Solver
+from file_reader.file_reader import FileReader
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
@@ -15,7 +16,6 @@ class TestDay4Solver(unittest.TestCase):
         result = Day4Solver().solve_first_problem('tests/resources/test_day_4_1.txt')
         self.assertEqual(result, 13)
 
-    @unittest.skip
     def test_can_solve_second_problem(self):
         result = Day4Solver().solve_second_problem('tests/resources/test_day_4_1.txt')
         self.assertEqual(result, 467835)
@@ -35,3 +35,25 @@ class TestDay4Solver(unittest.TestCase):
             'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'
         )
         self.assertEqual(card3.card_value, 0)
+
+    def test_won_correct_number_of_cards(self):
+        card1: Card = Day4Solver().parse_card(
+            'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+        )
+        self.assertEqual(card1.matching_numbers, 4)
+        self.assertEqual(Day4Solver().get_winning_cards(card1), [2, 3, 4, 5])
+
+    def test_get_total(self):
+        lines = FileReader().get_lines('tests/resources/test_day_4_1.txt')
+        cards = [Day4Solver().parse_card(line) for line in lines]
+        pile = CardPile(cards)
+
+        self.assertEqual(pile.get_total, 30)
+
+    def test_get_card_wins(self):
+        lines = FileReader().get_lines('tests/resources/test_day_4_1.txt')
+        cards = [Day4Solver().parse_card(line) for line in lines]
+        pile = CardPile(cards)
+
+        total_scratchpads = pile.total_scratchpads
+        self.assertEqual(total_scratchpads, 30)
