264b07962c68d4184ac098dba89a993799714920
Mila Berni <mila.berni@agilelab.it>
2023-12-14 17:38:18 +0100

    add puzzle 2 solution

--- day_14/main.py
+++ day_14/main.py
@@ -17,27 +17,34 @@ class Day14Solver(Solver):
 
             platform.append(temp_line)
 
-        platform = self.tilt_left(platform)
-        # platform = self.tilt_left(platform)
-
-        for line in platform:
-            logger.debug(line)
+        platform = self.tilt_west(self.transpose(platform))
+        platform = self.transpose(platform)
 
         rocks_count = self.count_rocks(platform)
         logger.debug(f'Total rocks after tilting is {rocks_count}')
         return rocks_count
 
-    def tilt_left(self, platform: list[list[str]]) -> list[list[str]]:
-        platform = self.transpose(platform)
+    def tilt_west(self, platform: list[list[str]]) -> list[list[str]]:
         for line in platform:
             for index in range(len(line)):
                 if line[index] != 'O':
                     continue
-                new_index = self.move_rock(line, index)
+                new_index = self.move_rock_left(line, index)
                 if index != new_index:
                     line[new_index] = line[index]
                     line[index] = '.'
-        return self.transpose(platform)
+        return platform
+
+    def tilt_east(self, platform: list[list[str]]) -> list[list[str]]:
+        for line in platform:
+            for index in range(len(line) - 1, -1, -1):
+                if line[index] != 'O':
+                    continue
+                new_index = self.move_rock_right(line, index)
+                if index != new_index:
+                    line[new_index] = line[index]
+                    line[index] = '.'
+        return platform
 
     def transpose(self, platform: list[list[str]]) -> list[list[str]]:
         return [
@@ -53,18 +60,77 @@ class Day14Solver(Solver):
             length -= 1
         return result
 
-    def move_rock(self, line: list[str], rock_index: int) -> int:
+    def move_rock_left(self, line: list[str], rock_index: int) -> int:
         diff = 0
         while rock_index - diff > 0 and line[rock_index - diff - 1] not in ['O', '#']:
             diff += 1
         return rock_index - diff
 
-    def solve_second_problem(self, file_name: str) -> int:
-        # lines = self.get_lines(file_name)
+    def move_rock_right(self, line: list[str], rock_index: int) -> int:
+        diff = 0
+        while rock_index + diff + 1 < len(line) and line[rock_index + diff + 1] not in [
+            'O',
+            '#',
+        ]:
+            diff += 1
+        return rock_index + diff
 
-        return 0
+    def solve_second_problem(self, file_name: str) -> int:
+        lines = self.get_lines(file_name)
+        platform: list[list[str]] = []
+
+        for row in range(len(lines)):
+            temp_line: list[str] = []
+            for column in range(len(lines[row])):
+                temp_line.append(lines[row][column])
+
+            platform.append(temp_line)
+
+        logger.debug('')
+        MAX_CYCLES = 1000000000
+
+        seen_platforms: dict[int, list[list[str]]] = {}
+        repeted_index = 0
+        interval_length = 0
+        for i in range(MAX_CYCLES):
+            platform = self.tilt_north(platform)
+            platform = self.tilt_west(platform)
+            platform = self.tilt_south(platform)
+            platform = self.tilt_east(platform)
+
+            for seen_index in seen_platforms.keys():
+                if seen_platforms[seen_index] == platform:
+                    repeted_index = seen_index
+                    interval_length = i - seen_index
+                    break
+
+            if repeted_index != 0:
+                break
+
+            seen_platforms[i] = platform
+            logger.info(f'At cycle {i} rocks count is {self.count_rocks(platform)}')
+
+        logger.debug(
+            f'Start of repetition is {repeted_index} '
+            + f'and interval length is {interval_length}'
+        )
+
+        billionth_index = (MAX_CYCLES - repeted_index) % interval_length
+        rocks_count = self.count_rocks(
+            seen_platforms[repeted_index + interval_length - billionth_index]
+        )
+        logger.info(f'The total after {MAX_CYCLES} cycles is {rocks_count}')
+        return rocks_count
+
+    def tilt_south(self, platform: list[list[str]]) -> list[list[str]]:
+        platform = self.tilt_east(self.transpose(platform))
+        return self.transpose(platform)
+
+    def tilt_north(self, platform: list[list[str]]) -> list[list[str]]:
+        platform = self.tilt_west(self.transpose(platform))
+        return self.transpose(platform)
 
 
 if __name__ == '__main__':
     Day14Solver().solve_first_problem("day_14/input.txt")
-    # Day14Solver().solve_second_problem("day_14/input.txt")
+    Day14Solver().solve_second_problem("day_14/input.txt")
--- tests/test_day_14.py
+++ tests/test_day_14.py
@@ -11,20 +11,27 @@ class TestDay14Solver(unittest.TestCase):
         result = Day14Solver().solve_first_problem('tests/resources/test_day_14.txt')
         self.assertEqual(result, 136)
 
-    @unittest.skip('tbd')
     def test_can_solve_second_problem(self):
         result = Day14Solver().solve_second_problem('tests/resources/test_day_14.txt')
-        self.assertEqual(result, 400)
+        self.assertEqual(result, 64)
 
     def test_rock_is_moved_correctly(self):
         line1 = ['O', 'O', '.', '#', '.', 'O', '.', '.', '#', '#']
-        new_index = Day14Solver().move_rock(line1, 5)
+        new_index = Day14Solver().move_rock_left(line1, 5)
         self.assertEqual(new_index, 4)
 
         line2 = ['O', 'O', '.', '#', '.', 'O', '.', '.', '#', '#']
-        new_index = Day14Solver().move_rock(line2, 1)
+        new_index = Day14Solver().move_rock_left(line2, 1)
         self.assertEqual(new_index, 1)
 
         line3 = ['.', '.', '.', '.', 'O', '#', '.', 'O', '#', '.']
-        new_index = Day14Solver().move_rock(line3, 4)
-        self.assertEqual(new_index, 0)
+        new_index = Day14Solver().move_rock_right(line3, 4)
+        self.assertEqual(new_index, 4)
+
+        line4 = ['.', '.', '.', '.', 'O', '#', '.', 'O', '#', 'O']
+        new_index = Day14Solver().move_rock_right(line4, 9)
+        self.assertEqual(new_index, 9)
+
+        line5 = ['.', 'O', '.', '.', '.', '#', '.', 'O', '#', 'O']
+        new_index = Day14Solver().move_rock_right(line5, 1)
+        self.assertEqual(new_index, 4)
