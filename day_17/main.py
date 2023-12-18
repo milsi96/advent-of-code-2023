ff0065fd88a7fe116f172c7b04a75ce3c107cfb5
Mila Berni <mila.berni@agilelab.it>
2023-12-18 20:26:04 +0100

    changed a bit the implementation but it still doesnt work

--- day_17/main.py
+++ day_17/main.py
@@ -1,61 +1,77 @@
-import sys
 from typing import Tuple
 from custom_logger.custom_logger import CustomLogger
 from solver.solver import Solver
 
 logger = CustomLogger(__name__).get_logger()
 
+Point = Tuple[int, int]
+
 
 class Day17Solver(Solver):
-    lowest_heat_loss: int = sys.maxsize
-
     def solve_first_problem(self, file_name: str) -> int:
-        lines = self.get_lines(file_name)
-        city_map: dict[Tuple[int, int], int] = {}
-        cities: list[list[int]] = [
-            [int(lines[row][column]) for column in range(len(lines[row]))]
-            for row in range(len(lines))
-        ]
+        city_map: dict[Point, int] = self.generate_map(self.get_lines(file_name))
 
-        for row in range(len(cities)):
-            for column in range(len(cities[row])):
-                city_map[(row, column)] = cities[row][column]
+        logger.debug(city_map)
 
-        start_point = (0, 0)
-        end_point = (len(lines) - 1, len(lines) - 1)
-        came_from, cost_so_far = self.dijkstra_search(city_map, start_point, end_point)
+        came_from, cost_so_far = self.dijkstra_search(city_map, (0, 0), (12, 12))
 
-        path = self.reconstruct_path(came_from, start_point, end_point)
-        logger.debug(", ".join(map(str, path)))
+        path = self.reconstruct_path(came_from, (0, 0), (12, 12))
 
-        return self.get_heat_loss(city_map, path)
+        logger.debug(" ".join(map(str, path)))
+
+        logger.debug(cost_so_far[(1, 5)])
+        logger.debug(cost_so_far[(2, 4)])
+
+        return cost_so_far[(12, 12)]
+
+    def generate_map(self, lines: list[str]) -> dict[Point, int]:
+        city_map: dict[Point, int] = {}
+        for row in range(len(lines)):
+            for column in range(len(lines[row])):
+                city_map[(row, column)] = int(lines[row][column])
+        return city_map
 
     def dijkstra_search(
-        self,
-        city_map: dict[Tuple[int, int], int],
-        start: Tuple[int, int],
-        end: Tuple[int, int],
-    ) -> Tuple[dict[Tuple[int, int], Tuple[int, int]], dict[Tuple[int, int], int]]:
-        frontier: list[Tuple[int, int]] = [start]
-        came_from: dict[Tuple[int, int], Tuple[int, int]] = {}
-        cost_so_far: dict[Tuple[int, int], int] = {}
+        self, city_map: dict[Point, int], start: Point, end: Point
+    ) -> Tuple[dict[Point, Point], dict[Point, int]]:
+        frontier: list[Point] = [start]
+        came_from: dict[Point, Point] = {}
+        cost_so_far: dict[Point, int] = {}
         cost_so_far[start] = 0
 
-        while len(frontier) > 0:
-            current: Tuple[int, int] = frontier.pop(0)
+        ROW = 0
+        COLUMN = 1
+
+        while len(frontier):
+            current: Point = frontier.pop(0)
+
+            last_five_points = self.reconstruct_path(came_from, start, current)[-5:]
+            if len(last_five_points) == 5:
+                if all([last_five_points[-1][ROW] == p[ROW] for p in last_five_points]):
+                    continue
+                if all(
+                    [
+                        last_five_points[-1][COLUMN] == p[COLUMN]
+                        for p in last_five_points
+                    ]
+                ):
+                    continue
 
             if current == end:
                 break
 
-            previous_points: list[Tuple[int, int]] = self.reconstruct_path(
-                came_from, start, current
-            )
-            next_points = self.get_next_points(city_map, previous_points, current)
+            next_points = [
+                (current[ROW], current[COLUMN] + 1),
+                (current[ROW], current[COLUMN] - 1),
+                (current[ROW] + 1, current[COLUMN]),
+                (current[ROW] - 1, current[COLUMN]),
+            ]
+
             for next in next_points:
-                if not self.path_is_admissable(previous_points + [next]):
+                if next not in city_map:
                     continue
                 new_cost = cost_so_far[current] + city_map[next]
-                if next not in cost_so_far.keys() or new_cost < cost_so_far[next]:
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     frontier.append(next)
                     came_from[next] = current
@@ -63,14 +79,11 @@ class Day17Solver(Solver):
         return came_from, cost_so_far
 
     def reconstruct_path(
-        self,
-        came_from: dict[Tuple[int, int], Tuple[int, int]],
-        start: Tuple[int, int],
-        end: Tuple[int, int],
-    ) -> list[Tuple[int, int]]:
-        current = end
-        path: list[Tuple[int, int]] = []
-        if end not in came_from.keys():
+        self, came_from: dict[Point, Point], start: Point, goal: Point
+    ) -> list[Point]:
+        current: Point = goal
+        path: list[Point] = []
+        if goal not in came_from:
             return []
         while current != start:
             path.append(current)
@@ -78,81 +91,6 @@ class Day17Solver(Solver):
         path.append(start)
         path.reverse()
         return path
-
-    def get_next_points(
-        self,
-        city_map: dict[Tuple[int, int], int],
-        previous_points: list[Tuple[int, int]],
-        point: Tuple[int, int],
-    ) -> list[Tuple[int, int]]:
-        ROW = 0
-        COLUMN = 1
-        next_points = [
-            (point[ROW], point[COLUMN] + 1),
-            (point[ROW], point[COLUMN] - 1),
-            (point[ROW] - 1, point[COLUMN]),
-            (point[ROW] + 1, point[COLUMN]),
-        ]
-
-        return list(
-            filter(
-                lambda p: p in city_map.keys() and p not in previous_points, next_points
-            )
-        )
-
-    def path_is_admissable(self, points: list[Tuple[int, int]]) -> bool:
-        ROW, COLUMN = 0, 1
-
-        i = 0
-        max_consequent_cubes = 5
-        while i + max_consequent_cubes < len(points):
-            if all(
-                [p[ROW] == points[i][ROW] for p in points[i : i + max_consequent_cubes]]
-            ):
-                return False
-            elif all(
-                [
-                    p[COLUMN] == points[i][COLUMN]
-                    for p in points[i : i + max_consequent_cubes]
-                ]
-            ):
-                return False
-
-            i += 1
-
-        return True
-
-    def generate_heat_loss(
-        self,
-        city_map: dict[Tuple[int, int], int],
-        previous_points: list[Tuple[int, int]],
-        start: Tuple[int, int],
-        end: Tuple[int, int],
-    ) -> None:
-        logger.debug(f'Trying all points from {start}')
-
-        previous_points.append(start)
-        next_points = self.get_next_points(city_map, previous_points, start)
-        for point in next_points:
-            if point == end:
-                self.lowest_heat_loss = min(
-                    self.get_heat_loss(city_map, previous_points) + city_map[end],
-                    self.lowest_heat_loss,
-                )
-                logger.debug(f'Current lowest heat loss is {self.lowest_heat_loss}')
-            else:
-                if (
-                    self.get_heat_loss(city_map, previous_points + [point])
-                    > self.lowest_heat_loss
-                ):
-                    continue
-                new_path = previous_points.copy()
-                self.generate_heat_loss(city_map, new_path, point, end)
-
-    def get_heat_loss(
-        self, city_map: dict[Tuple[int, int], int], path: list[Tuple[int, int]]
-    ) -> int:
-        return sum([city_map[p] for p in path if p != (0, 0)])
 
     def solve_second_problem(self, file_name: str) -> int:
         return 0
--- tests/test_day_17.py
+++ tests/test_day_17.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 import unittest
 
 from custom_logger.custom_logger import CustomLogger
@@ -9,63 +8,10 @@ logger = CustomLogger(__name__).get_logger()
 
 class TestDay17Solver(unittest.TestCase):
     def setUp(self) -> None:
-        lines = Day17Solver().get_lines('tests/resources/test_day_17.txt')
-        city_map: dict[Tuple[int, int], int] = {}
-        cities: list[list[int]] = [
-            [int(lines[row][column]) for column in range(len(lines[row]))]
-            for row in range(len(lines))
-        ]
-
-        for row in range(len(cities)):
-            for column in range(len(cities[row])):
-                city_map[(row, column)] = cities[row][column]
-
-        self.city_map = city_map
+        self.city_map = Day17Solver().generate_map(
+            Day17Solver().get_lines('tests/resources/test_day_17.txt')
+        )
 
     def test_can_solve_first_problem(self) -> None:
         result = Day17Solver().solve_first_problem('tests/resources/test_day_17.txt')
         self.assertEqual(result, 102)
-
-    def test_fewest_heat_loss(self) -> None:
-        path = [
-            (0, 0),
-            (0, 1),
-            (0, 2),
-            (1, 2),
-            (1, 3),
-            (1, 4),
-            (1, 5),
-            (0, 5),
-            (0, 6),
-            (0, 7),
-            (0, 8),
-            (1, 8),
-            (2, 8),
-            (2, 9),
-            (2, 10),
-            (3, 10),
-            (4, 10),
-            (4, 11),
-            (5, 11),
-            (6, 11),
-            (7, 11),
-            (7, 12),
-            (8, 12),
-            (9, 12),
-            (10, 12),
-            (10, 11),
-            (11, 11),
-            (12, 11),
-            (12, 12),
-        ]
-        heat_loss = Day17Solver().get_heat_loss(self.city_map, path)
-        self.assertEqual(heat_loss, 102)
-
-    def test_path_is_admissable(self) -> None:
-        path1 = [(0, 0), (0, 1), (0, 2), (0, 3), (1, 3)]
-        admissable = Day17Solver().path_is_admissable(path1)
-        self.assertEqual(admissable, True)
-
-        path2 = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5)]
-        admissable = Day17Solver().path_is_admissable(path2)
-        self.assertEqual(admissable, False)
